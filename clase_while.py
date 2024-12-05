bc0467534fd730fb58834367880e23123d4b68cf
Santiago Mesa <thiagomesa21@gmail.com>
2024-12-04 19:23:01 -0500

    primeros ejercicios
    primeros ejercicios de la explicacion.

--- clase_while.py
+++ clase_while.py
@@ -3,4 +3,29 @@
 #while condicion:
     #bloque de codigo a repetir
 
+#definir una variable que lleve el control del ciclo
+# numero = 1
 
+# while numero <= 5: #condicion para iterar n veces...
+#     print(numero)
+#     numero += 1
+
+
+#contaremos hacia atras
+# numero = 10
+
+# while numero >= 1:
+#     print(numero)
+#     numero -= 1
+
+#Crear un programa que sume números ingresados por el usuario hasta que el usuario ingrese 0.
+
+# suma = 0
+
+# numero = int(input("Ingresa un numero o pulsa 0 para salir: "))
+
+# while numero != 0:
+#     suma += numero
+#     numero = int(input("Ingresa un numero o pulsa 0 para salir: "))
+
+# print(f"La suma total es: {suma}")
