80b1d3cec7ceac376a7af15341ce548c595d60a9
Santiago Mesa <thiagomesa21@gmail.com>
2024-12-04 19:55:34 -0500

    final de los ejercicios de clase
    ultimos ejercicios

--- clase_while.py
+++ clase_while.py
@@ -29,3 +29,41 @@
 #     numero = int(input("Ingresa un numero o pulsa 0 para salir: "))
 
 # print(f"La suma total es: {suma}")
+
+
+#condiciones dinamicas:
+#son aleatorias, y pueden cambiar con la ejecucion del codigo.
+
+#simulacion basada en una condicion externa.
+# Simularemos el cremimiento de una poblacion hasta que alcance un limite.
+
+# poblacion = 1000
+# limite = 5000
+# tasa_crecimiento = 1.1
+# anio = 1
+
+# while poblacion < limite:
+#     print(f"En el año {anio}, la Poblacion actual es de: {poblacion}")
+#     poblacion = int(poblacion * tasa_crecimiento)
+#     anio += 1
+
+# print(f"Poblacion final alcanzada: {poblacion}")
+
+#lecturas de un sensor...
+# simular la lectura de un sensor que medirá valores aleatorios hasta que alcance un valor objetivo.
+
+# import random
+
+# sensor = random.randint(0, 50)
+# objetivo = 40
+# contador = 1
+
+
+# while sensor < objetivo:
+#     print(f"en la lectura numero {contador}, el valor del sensor es: {sensor}")
+#     sensor = random.randint(0, 50)
+#     contador += 1
+
+# print(f"la lectura final alzanzada fue: {sensor}")
+
+
